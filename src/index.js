68c4670d8c86ad5193fbe496a0c60aed3140cb10
xumenger <xumenger@126.com>
2018-06-19 11:18:59 +0800

    image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './app.vue'
 
+import './assets/styles/test.css'
+import './assets/images/bg.jpg'
+
 const root = document.createElement('div')
 document.body.appendChild(root)
 
