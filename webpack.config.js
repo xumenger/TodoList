b1e073d994f705eddb5ed40666d06db5604ddada
xumenger <xumenger@126.com>
2018-06-19 11:03:28 +0800

    VueLoaderPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
-  output:{
+  output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
+  },
+  plugins: [
+    // make sure to include the plugin for the magic
+    new VueLoaderPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
   }
 }
