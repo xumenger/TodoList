ce1780449764c541167455ef09b1c1dd9581d9c6
xumenger <xumenger@126.com>
2018-06-20 14:53:48 +0800

    背景

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,7 @@
 <template>
-
+  <div id="app">
+    <div id="cover"></div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +10,22 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="stylus" scoped>
+#app{
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+}
+#cover{
+  position absolute
+  left 0
+  top 0
+  right 0
+  bottom 0
+  background-color #999
+  opacity .9
+  z-index -1
+}
 </style>
